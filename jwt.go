7042477cb31b4a8f44380220967ab157128e57c6
marketconnect <ipbogachenko@yandex.ru>
2024-10-05 10:58:59 +0300

    added end dated to the UserClaims

--- jwt.go
+++ jwt.go
@@ -16,26 +16,28 @@ type UserClaims struct {
 	jwt.StandardClaims
 	UserId       uint64 `json:"userId"`
 	Subscription string `json:"subscription"`
+	EndDate      string `json:"endDate"`
 }
 
 func NewJWTManager(secretKey string, tokenDuration time.Duration) *JWTManager {
 	return &JWTManager{secretKey, tokenDuration}
 }
 
-func (manager *JWTManager) Generate(userId uint64, subscription string) (string, error) {
+func (manager *JWTManager) Generate(userId uint64, subscription string, endDate string) (string, error) {
 	claims := UserClaims{
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: time.Now().Add(manager.tokenDuration).Unix(),
 		},
 		UserId:       userId,
 		Subscription: subscription,
+		EndDate:      endDate,
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(manager.secretKey))
 }
 
-func (manager *JWTManager) Verify(accessToken string) (*uint64, *string, error) {
+func (manager *JWTManager) Verify(accessToken string) (*uint64, *string, *string, error) {
 	token, err := jwt.ParseWithClaims(
 		accessToken,
 		&UserClaims{},
@@ -49,13 +51,13 @@ func (manager *JWTManager) Verify(accessToken string) (*uint64, *string, error) 
 	)
 
 	if err != nil {
-		return nil, nil, fmt.Errorf("invalid token: %w", err)
+		return nil, nil, nil, fmt.Errorf("invalid token: %w", err)
 	}
 
 	claims, ok := token.Claims.(*UserClaims)
 	if !ok {
-		return nil, nil, fmt.Errorf("invalid token claims")
+		return nil, nil, nil, fmt.Errorf("invalid token claims")
 	}
 
-	return &claims.UserId, &claims.Subscription, nil
+	return &claims.UserId, &claims.Subscription, &claims.EndDate, nil
 }
